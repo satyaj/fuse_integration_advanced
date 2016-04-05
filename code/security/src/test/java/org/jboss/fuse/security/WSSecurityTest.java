eb2075aaee3de029566cddc5476363668630db64
Charles Moulliard <cmoulliard@apache.org>
2016-04-05 17:14:28 +0200

    Rename file to append Test at the end

--- code/security/src/test/java/org/jboss/fuse/security/WSSecurityTest.java
+++ code/security/src/test/java/org/jboss/fuse/security/WSSecurityTest.java
@@ -23,7 +23,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class WSSecurity {
+public class WSSecurityTest {
 
     private WSS4JInInterceptor wsIn;
     private WSS4JOutInterceptor wsOut;
