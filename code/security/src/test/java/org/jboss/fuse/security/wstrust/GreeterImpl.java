abf94368943967f6b886c2fc8d099986931f7a7a
Charles Moulliard <cmoulliard@apache.org>
2016-04-08 19:01:26 +0200

    Add WS Trust example. There is a bug to fix as the Client can't talk to STS server

---
+++ code/security/src/test/java/org/jboss/fuse/security/wstrust/GreeterImpl.java
@@ -0,0 +1,23 @@
+package org.jboss.fuse.security.wstrust;
+
+import java.util.logging.Logger;
+import org.apache.hello_world_soap_http.Greeter;
+
+@javax.jws.WebService(name = "Greeter", serviceName = "SOAPService", 
+                      targetNamespace = "http://apache.org/hello_world_soap_http", 
+                      wsdlLocation = "file:./wsdl/hello_world.wsdl")
+public class GreeterImpl implements Greeter {
+
+    private static final Logger LOG = 
+        Logger.getLogger(GreeterImpl.class.getPackage().getName());
+    
+    /* (non-Javadoc)
+     * @see org.objectweb.hello_world_soap_http.Greeter#greetMe(java.lang.String)
+     */
+    public String greetMe(String me) {
+        LOG.info("Executing operation greetMe");
+        System.out.println("Executing operation greetMe");
+        System.out.println("Message received: " + me + "\n");
+        return "Hello " + me;
+    }
+}
---
+++ code/security/src/test/java/org/jboss/fuse/security/wstrust/WSTrustTest.java
@@ -0,0 +1,76 @@
+package org.jboss.fuse.security.wstrust;
+
+import org.apache.cxf.Bus;
+import org.apache.cxf.bus.spring.SpringBusFactory;
+import org.apache.cxf.testutil.common.AbstractBusClientServerTestBase;
+import org.jboss.fuse.security.SecurityTestUtil;
+import org.jboss.fuse.security.Server;
+import org.apache.hello_world_soap_http.Greeter;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+import java.net.URL;
+
+public class WSTrustTest extends AbstractBusClientServerTestBase {
+
+    private static final QName SERVICE_NAME
+            = new QName("http://apache.org/hello_world_soap_http", "SOAPService");
+
+    private static final QName PORT_NAME =
+            new QName("http://apache.org/hello_world_soap_http", "SoapPort");
+
+    @BeforeClass
+    public static void startServers() throws Exception {
+        assertTrue(
+                "STS Server failed to launch",
+                // run the server in the same process
+                // set this to false to fork
+                launchServer(Server.class, null, new String[] { "/org/jboss/fuse/security/wstrust/wssec-sts.xml" }, true));
+        assertTrue(
+                "Server failed to launch",
+                // run the server in the same process
+                // set this to false to fork
+                launchServer(Server.class, null, new String[] { "/org/jboss/fuse/security/wstrust/wssec-server.xml" }, true));
+    }
+
+    @AfterClass
+    public static void cleanup() throws Exception {
+        SecurityTestUtil.cleanup();
+        stopAllServers();
+    }
+
+    @Test
+    public void testSimpleClient() throws Exception {
+
+        SpringBusFactory bf = new SpringBusFactory();
+        URL busFile = WSTrustTest.class.getResource("wssec-client.xml");
+
+        Bus bus = bf.createBus(busFile.toString());
+        SpringBusFactory.setDefaultBus(bus);
+        SpringBusFactory.setThreadDefaultBus(bus);
+
+        URL wsdlURL = WSTrustTest.class.getResource("hello_world.wsdl");
+
+        Service service = Service.create(wsdlURL, SERVICE_NAME);
+        Greeter port = service.getPort(PORT_NAME, Greeter.class);
+
+        System.out.println("Invoking greetMe...");
+        try {
+            String resp = port.greetMe(System.getProperty("user.name"));
+            System.out.println("Server responded with: " + resp);
+            System.out.println();
+
+        } catch (Exception e) {
+            System.out.println("Invocation failed with the following: " + e.getCause());
+            System.out.println();
+        }
+
+        ((java.io.Closeable)port).close();
+        bus.shutdown(true);
+    }
+}
+
