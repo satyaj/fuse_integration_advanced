4cc86d5fb33802f7b82c8fc9f199542a4f4ce408
Charles Moulliard <cmoulliard@apache.org>
2016-04-11 15:34:01 +0200

    Add encrypt/decrypt XML payload example

--- code/security/src/test/java/org/jboss/fuse/security/encryption/EncryptDecryptPayloadTest.java
+++ code/security/src/test/java/org/jboss/fuse/security/encryption/EncryptDecryptPayloadTest.java
@@ -1,4 +1,21 @@
 package org.jboss.fuse.security.encryption;
 
-public class EncryptDecryptPayloadTest {
+import org.apache.camel.test.spring.CamelSpringTestSupport;
+import org.junit.Test;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class EncryptDecryptPayloadTest extends CamelSpringTestSupport {
+
+    TestHelper testHelper = new TestHelper();
+
+    protected AbstractApplicationContext createApplicationContext() {
+        return new ClassPathXmlApplicationContext(
+                "org/jboss/fuse/security/encryption/EncryptDecryptPayloadTest-context.xml");
+    }
+
+    @Test
+    public void testPayloadAsymmetricKeyDecryption() throws Exception {
+        testHelper.testDecryption(TestHelper.NS_XML_FRAGMENT, context());
+    }
 }
