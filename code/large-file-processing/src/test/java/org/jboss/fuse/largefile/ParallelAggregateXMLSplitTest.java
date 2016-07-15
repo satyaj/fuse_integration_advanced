0b6b448bb7101dcf44e989c0ebc785581c233166
Charles Moulliard <cmoulliard@apache.org>
2016-07-15 17:28:29 +0200

    Draft of improve speed

--- code/large-file-processing/src/test/java/org/jboss/fuse/largefile/ParallelAggregateXMLSplitTest.java
+++ code/large-file-processing/src/test/java/org/jboss/fuse/largefile/ParallelAggregateXMLSplitTest.java
@@ -13,16 +13,14 @@ import org.junit.Test;
 
 import java.util.List;
 
-public class ParallelXMLSplitTest extends CamelTestSupport {
+public class ParallelAggregateXMLSplitTest extends CamelTestSupport {
 
-    @Override
-    public void setUp() throws Exception {
+    @Override public void setUp() throws Exception {
         deleteDirectory("target/xtokenize");
         super.setUp();
     }
 
-    @Test
-    public void testParallel() throws Exception {
+    @Test public void testParallel() throws Exception {
         String result = "a1,b2,c3,d4,e5,f6";
         MockEndpoint mock = getMockEndpoint("mock:result");
         mock.expectedMessageCount(1);
@@ -33,25 +31,20 @@ public class ParallelXMLSplitTest extends CamelTestSupport {
         assertMockEndpointsSatisfied();
     }
 
-    @Override
-    protected RouteBuilder createRouteBuilder() {
+    @Override protected RouteBuilder createRouteBuilder() {
 
         return new RouteBuilder() {
 
             private int counter = 1;
 
             public void configure() {
-                from("direct:start")
-                    .split().tokenize(",").aggregationStrategy(new MyAggregationStrategy())
-                      .process(new Processor() {
-                          @Override public void process(Exchange exchange) throws Exception {
-                              String body = (String)exchange.getIn().getBody();
-                              exchange.getOut().setBody(body + Integer.toString(counter++));
-                          }
-                      })
-                      .log(">> Splitted msg : ${body}")
-                      .end()
-                    .to("mock:result");
+                from("direct:start").split().tokenize(",").aggregationStrategy(new MyAggregationStrategy())
+                        .process(new Processor() {
+                            @Override public void process(Exchange exchange) throws Exception {
+                                String body = (String) exchange.getIn().getBody();
+                                exchange.getOut().setBody(body + Integer.toString(counter++));
+                            }
+                        }).log(">> Splitted msg : ${body}").end().to("mock:result");
 
             }
         };
@@ -61,15 +54,13 @@ public class ParallelXMLSplitTest extends CamelTestSupport {
 
         @Override public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
             if (oldExchange == null) {
+                newExchange.getIn().setBody(new StringBuilder(newExchange.getIn().getBody(String.class)));
                 return newExchange;
             }
-
-            String oldBody = oldExchange.getIn().getBody(String.class);
-            String newBody = newExchange.getIn().getBody(String.class);
-            oldExchange.getIn().setBody(oldBody + "," + newBody);
-
+            oldExchange.getIn().getBody(StringBuilder.class)
+                    .append(",")
+                    .append(newExchange.getIn().getBody(String.class));
             return oldExchange;
         }
     }
-
 }
